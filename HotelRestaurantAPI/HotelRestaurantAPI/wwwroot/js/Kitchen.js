d2ba93b53484450d91f055ffa37eeda52733993f
au618503 <58029607+au618503@users.noreply.github.com>
2022-11-15 18:40:19 +0100

    SignalR

---
+++ HotelRestaurantAPI/HotelRestaurantAPI/wwwroot/js/Kitchen.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/KitchenService").build();
+
+connection.on("kitchenUpdate", function () {
+
+    window.location.reload("kitchen");
+});
+
+connection.start().then(function () {
+    console.log("Connected");
+}).catch(function (err) {
+    console.error(err.toString());
+});
+
