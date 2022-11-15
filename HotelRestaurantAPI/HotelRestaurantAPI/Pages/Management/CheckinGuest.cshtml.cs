9a838e116d3f027c43cab0299aeb72f2700323a2
Marcin Szymanek <202009418@post.au.dk>
2022-11-15 12:20:04 +0100

    Changed database simplified checkin

---
+++ HotelRestaurantAPI/HotelRestaurantAPI/Pages/Management/CheckinGuest.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HotelRestaurantAPI.Data;
+using HotelRestaurantAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HotelRestaurantAPI.Pages.Management
+{
+    [Authorize(Policy = "WaiterStaff")]
+    public class CheckinGuestModel : PageModel
+    {
+        private readonly HotelRestaurantAPI.Data.HotelDataContext _context;
+
+        public CheckinGuestModel(HotelRestaurantAPI.Data.HotelDataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public CheckedIn CheckedIn { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.CheckedIn.Add(CheckedIn);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
