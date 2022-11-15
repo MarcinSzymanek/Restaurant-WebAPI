81b6a81dd483b0c5d91e753ea706ee497f9e81c2
au618503 <58029607+au618503@users.noreply.github.com>
2022-11-15 15:21:25 +0100

    Kitchen files created

---
+++ HotelRestaurantAPI/HotelRestaurantAPI/Services/KitchenService.cs
@@ -0,0 +1,13 @@
+﻿using HotelRestaurantAPI.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HotelRestaurantAPI.Services
+{
+    public class KitchenService : Hub<IKitchenService>
+    {
+        public async Task KitchenUpdate()
+        {
+            await Clients.All.KitchenUpdate();
+        }
+    }
+}
